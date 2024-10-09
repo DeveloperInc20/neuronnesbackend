4dc4f1217ad22726b0ff98148cfe7e32885ed1b4
Boris Kendrick <boriskendrick@gmail.com>
2024-10-09 12:59:09 +0000

    Gestion route :auth logout et des posts

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 ## Neuronnes Technologies documentation
+
+**ETAPE 1: Création des migrations: users, posts**
+
+**ETAPE 2:**
+- Création de routes API AUTH : login, register, logout (POST METHOD)
+- Création de routes API POST: 
+a- Création de post, 
+b-liste des posts, 
+c- detail d’un post du user connecté
+c- detail d’un post du user connecté (à partir du slug)
+d- mise à jour d’un post du user connecté
+e- supprimer d’un post qui appartient au user connecté
--- routes/api.php
+++ routes/api.php
@@ -18,11 +18,24 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('users')->group(function () {
-    Route::prefix('auth')->group(function () {
-        // CRÉATION DE COMPTE
-        Route::post('/register',[App\Http\Controllers\Auth\RegisterController::class,"register"]);
-        // CONNEXION AU COMPTE
-        Route::post('/login',[App\Http\Controllers\Auth\LoginController::class,"login"]);
-    });
+// AUTHENTIFICATION PREFIX 
+Route::prefix('auth')->group(function () {
+    // création de compte
+    Route::post('/register',[App\Http\Controllers\Auth\RegisterController::class,"register"]);
+    // connexion au compte
+    Route::post('/login',[App\Http\Controllers\Auth\LoginController::class,"login"]);
+    // déconnexion au compte
+    Route::delete('/logout',[App\Http\Controllers\Auth\LoginController::class,"logout"]);
 });
+// création des posts
+Route::post('/posts',[App\Http\Controllers\PostController::class,"store"]);
+// liste des posts du user connecté
+Route::get('/posts',[App\Http\Controllers\PostController::class,"index"]);
+//detail d’un post du user connecté (à partir de l’id)
+Route::get('/posts/{id}',[App\Http\Controllers\PostController::class,"show"]);
+//detail d’un post du user connecté (à partir du slug)
+Route::get('/posts/{slug}',[App\Http\Controllers\PostController::class,"show_slug"]);
+//mise à jour d’un post du user connecté
+Route::put('/posts/{id}',[App\Http\Controllers\PostController::class,"update"]);
+//supprimer d’un post qui appartient au user connecté
+Route::delete('/posts/{id}',[App\Http\Controllers\PostController::class,"destroy"]);
